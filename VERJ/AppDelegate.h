226e7497a465a3618eb1d2df7628302613006ff9
Daniel Ho <danielho@Daniels-MacBook-Air-6.local>
2014-11-15 20:03:08 -0800

    Initial Commit

---
+++ VERJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VERJ
+//
+//  Created by Daniel Ho on 11/15/14.
+//  Copyright (c) 2014 Verj Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VERJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VERJ
+//
+//  Created by Daniel Ho on 11/15/14.
+//  Copyright (c) 2014 Verj Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
